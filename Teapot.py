ee5633de8734f73880353cc6e59170938d5a6ecd
Dremixbotdev <78578772+Dremixbotdev@users.noreply.github.com>
2021-06-01 09:12:33 +0530

    Add cmds via cogs

--- Teapot.py
+++ Teapot.py
@@ -1,11 +1,13 @@
+# If there are any errors, please contact me at Sibi Agilan#2044
+
+
 import os
 import time
-from os.path import join, dirname
 import json
 import requests
 
 import discord
-from discord.ext import commands as dcmd
+from discord.ext import commands
 from dotenv import load_dotenv
 
 import teapot
@@ -50,7 +52,7 @@ else:
     print("An unknown error has occurred when fetching the latest Teapot.py version\n")
     print("HTML Error Code:" + str(req.status_code))
 
-load_dotenv(join(dirname(__file__), '.env'))
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 if os.getenv('CONFIG_VERSION') != teapot.config_version():
     if os.path.isfile('.env'):
@@ -86,28 +88,27 @@ if teapot.config.storage_type() == "mysql": # if .env use mysql, create the tabl
 intents = discord.Intents.default()
 intents.members = True
 intents.typing = False
-bot = dcmd.Bot(intents=intents, command_prefix=teapot.config.bot_prefix(), help_command=None)
+bot = commands.Bot(intents=intents,
+                   command_prefix=teapot.config.bot_prefix(),
+                   help_command=None,                                   # https://gist.github.com/InterStella0/b78488fb28cadf279dfd3164b9f0cf96
+                   status=discord.Status.online,                        # Update status
+                   activity=discord.Game(teapot.config.bot_status()))
+
 
 @bot.event
 async def on_ready():
     print(f"Connected to DiscordAPI in {round(time.perf_counter() - discord_time_start, 2)}s")
-    time_start = time.perf_counter()
-    teapot.events.__init__(bot)
-    teapot.cogs.cmds.__init__(bot)
-    teapot.cogs.music.setup(bot)
-    teapot.cogs.osu.setup(bot)
-    teapot.cogs.github.setup(bot)
-    teapot.cogs.cat.setup(bot)
-    teapot.cogs.neko.setup(bot)
     if teapot.config.storage_type() == "mysql":
         for guild in bot.guilds:
             teapot.managers.database.create_guild_table(guild)
     elif teapot.config.storage_type() == "sqlite":
         print("[!] Warning: SQLite storage has not been implemented yet. MySQL is recommended")  # WIP
-    print(f"Registered commands and events in {round(time.perf_counter() - time_start, 2)}s")
-    await bot.change_presence(status=discord.Status.online,
-                              activity=discord.Game(teapot.config.bot_status()))  # Update Bot status
 
+
+for fname in os.listdir('./Teapot/cogs'):
+  if fname.endswith('.py'):
+    bot.load_extension(f'cogs.{fname[:-3]}')
+        
 
 try:
     discord_time_start = time.perf_counter()
